3c95d360876b2c09068acd63334d8361643ff8b1
unknown <libb4@LIBB4-B36226320.lenovo.com>
2015-11-03 16:41:33 +0800

    brand list

--- view/admin/brand.py
+++ view/admin/brand.py
@@ -13,10 +13,24 @@ class BrandListHandler(BaseHandler):
 class BrandHandler(BaseHandler):
 #     @authenticated
     def get(self, *args, **kwargs):
-        self.render('addbrand.html',result = '')
+        
+        id = self.get_argument('id',0)
+        id = int(id)
+        if id>0 :
+           name =  get_brand_by_id(id) 
+           self.render('addbrand.html',id=id,name=name,result = '')
+        else :
+           self.render('addbrand.html',id=0,name='',result = '')           
+       
 
     def post(self, *args, **kwargs):
-        name = self.get_argument('name')
-        add_brand(name)
-        self.render('addbrand.html',result = 'save succeeded')
+        id = self.get_argument('id',0)
+        name = self.get_argument('name','')
+        
+        if int(id)>0:
+            update_brand(id,name)
+        else :
+            add_brand(name)
+        Brand = get_all_brand()
+        self.render('brandlist.html',brand = Brand,result = 'succeeded')
 
